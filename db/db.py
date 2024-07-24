0d667932e1b30e5c4daf6ad68270fc98c1f69818
gabenoh <mixabuchko@gmail.com>
2024-07-24 15:11:33 +0300

    add retry decorator

--- db/db.py
+++ db/db.py
@@ -1,13 +1,12 @@
 from typing import List, Dict
-from sqlalchemy import Column, Integer, String, create_engine, delete, update
+from sqlalchemy import Column, Integer, String, create_engine, delete, update, select
 from sqlalchemy.orm import declarative_base, sessionmaker
 from sqlalchemy.exc import SQLAlchemyError, OperationalError
 import mysql.connector
-from constants import ENGINE
 import logging
 import time
 from functools import wraps
-
+from constants import ENGINE
 
 # Створення об'єкта Base
 Base = declarative_base()
@@ -15,23 +14,6 @@ Base = declarative_base()
 logging.basicConfig(filename='/home/galmed/svitlograf/logs/svitlo.log', level=logging.INFO,
                     format='%(asctime)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
-
-
-def retry_on_failure(retries=3, delay=5):
-    def decorator(func):
-        @wraps(func)
-        def wrapper(*args, **kwargs):
-            for attempt in range(retries):
-                try:
-                    return func(*args, **kwargs)
-                except (OperationalError, mysql.connector.errors.OperationalError) as e:
-                    logger.error(f"Помилка при виконанні функції {func.__name__}: {e}")
-                    if attempt < retries - 1:
-                        time.sleep(delay)
-                    else:
-                        raise
-        return wrapper
-    return decorator
 
 
 # Визначення моделі Svitlo
@@ -49,13 +31,39 @@ engine = create_engine(ENGINE)
 Base.metadata.create_all(engine)
 
 # Створення сесії
-Session = sessionmaker(bind=engine)
+SessionLocal = sessionmaker(bind=engine)
+
+
+def retry_on_failure(retries=3, delay=5):
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            for attempt in range(retries):
+                session = SessionLocal()
+                try:
+                    return func(session, *args, **kwargs)
+                except (OperationalError, mysql.connector.errors.OperationalError) as e:
+                    logger.error(f"Помилка при виконанні функції {func.__name__}: {e}")
+                    session.close()
+                    if attempt < retries - 1:
+                        time.sleep(delay)
+                    else:
+                        raise
+                except SQLAlchemyError as e:
+                    session.rollback()
+                    logger.error(f"SQLAlchemy помилка при виконанні функції {func.__name__}: {e}")
+                    raise
+                finally:
+                    session.close()
+
+        return wrapper
+
+    return decorator
 
 
 @retry_on_failure()
-def add_user(user_id: str, turn: str) -> None:
+def add_user(session, user_id: str, turn: str) -> None:
     """Додавання нового користувача"""
-    session = Session()
     try:
         new_user = Svitlo(user=user_id, turn=turn)
         session.add(new_user)
@@ -63,31 +71,25 @@ def add_user(user_id: str, turn: str) -> None:
     except SQLAlchemyError as e:
         session.rollback()
         logger.error(f"Помилка при додаванні користувача: {e}")
-    finally:
-        session.close()
 
 
 @retry_on_failure()
-def check_user(user_id: str, turn: str) -> None:
+def check_user(session, user_id: str, turn: str) -> None:
     """Перевірка користувача"""
-    session = Session()
     try:
         user = session.query(Svitlo).filter(Svitlo.user == user_id).first()
         if not user:
-            add_user(user_id, turn)
+            add_user(session, user_id, turn)
         else:
-            update_user_turn(user_id, turn)
+            update_user_turn(session, user_id, turn)
     except SQLAlchemyError as e:
         session.rollback()
         logger.error(f"Помилка при перевірці користувача: {e}")
-    finally:
-        session.close()
 
 
 @retry_on_failure()
-def update_user_turn(user_id: int, turn: str) -> None:
+def update_user_turn(session, user_id: str, turn: str) -> None:
     """Редагування наявного користувача"""
-    session = Session()
     try:
         stmt = (
             update(Svitlo)
@@ -99,41 +101,33 @@ def update_user_turn(user_id: int, turn: str) -> None:
     except SQLAlchemyError as e:
         session.rollback()
         logger.error(f"Помилка при оновленні користувача: {e}")
-    finally:
-        session.close()
 
 
 @retry_on_failure()
-def get_all_user() -> List[Dict[str, str]]:
+def get_all_user(session) -> List[Dict[str, str]]:
     """Отримання всіх користувачів з бази даних"""
-    session = Session()
     try:
         user_list = session.query(Svitlo).all()
         return [{'id': str(user.id), 'user': user.user, 'turn': user.turn} for user in user_list]
     except SQLAlchemyError as e:
         logger.error(f"Помилка при виводу всіх користувачів {e}")
         return []
-    finally:
-        session.close()
 
 
 @retry_on_failure()
-def delete_user(user_id: int) -> None:
+def delete_user(session, user_id: int) -> None:
     """Видалення користувача з таблиці за індексом"""
-    session = Session()
     try:
         stmt = delete(Svitlo).where(Svitlo.id == user_id)
         session.execute(stmt)
         session.commit()
     except SQLAlchemyError as e:
         session.rollback()
-        print(f"Помилка при видаленні користувача: {e}")
-    finally:
-        session.close()
+        logger.error(f"Помилка при видаленні користувача: {e}")
 
 
+# Приклад використання
 if __name__ == '__main__':
     user_list = get_all_user()
     for user in user_list:
         print(user)
-
